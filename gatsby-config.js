5b84ced32bfe27607ecca4d6df2342d067c76e80
Clemens Heithecker <clemens@heithecker.dev>
2020-08-13 15:33:48 +0200

    Add manifest and favicon
     - Remove default Gatsby favicon
     - Add and configure gatsby-plugin-manifest
     - Add custom favicon with CSS media query to change the icon color
       when the system color scheme is set to dark

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: [`gatsby-plugin-emotion`],
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Labs Gatsby Nagivation`,
+        short_name: `Labs-Nav`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#1C1C1E`,
+        display: `browser`,
+        icon: `src/images/logo.svg`,
+        // ToDo: Change theme_color based on color scheme
+        // theme_color_in_head: false,
+      },
+    },
+  ],
 }
